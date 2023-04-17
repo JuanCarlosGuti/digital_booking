354f4f78da4f4cc78df13cb1f9c094ba09fa2b44
Juan Carlos Gutierrez <juancgutierrezh@gmail.com>
2023-04-17 18:45:52 -0500

    first commit

---
+++ frontend/src/components/Body/Booking/BookingForm.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useReducer } from "react";
+import "./Booking.scss";
+import { getUserByToken } from "../../../services/fetchService";
+import { useState } from "react";
+
+export default function BookingForm(props) {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const [ready, setReady] = useState(false);
+
+  return (
+    <div className="formContainer">
+      <h3>Completá tus datos</h3>
+      <form action="">
+        <div className="formBlock">
+          <label htmlFor="">Nombre</label>
+          <input
+            type="text"
+            className="formInput"
+            id="nombreInput"
+            value={user.name}
+            disabled
+          />
+        </div>
+
+        <div className="formBlock">
+          <label htmlFor="">Apellido</label>
+          <input
+            type="text"
+            className="formInput"
+            id="apellidoInput"
+            value={user.lastname}
+            disabled
+          />
+        </div>
+        <div className="formBlock">
+          <label htmlFor="">Correo electrónico</label>
+          <input
+            type="email"
+            className="formInput"
+            id="correoInput"
+            value={user.email}
+            disabled
+          />
+        </div>
+        <div className="formBlock">
+          <label htmlFor="">Ciudad</label>
+          <input
+            type="text"
+            className="formInput"
+            id="ciudadInput"
+            value={props.city.city + "," + props.city.country}
+            disabled
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
